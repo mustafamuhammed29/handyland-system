064eb19e6c8bdca1588ef02b0a47b23fb8069e5d
Mustafa Muhammed <83115972+mustafamuhammed29@users.noreply.github.com>
2026-08-04 18:15:17 +0200

    update: manual update via start script

--- vite.config.js
+++ vite.config.js
@@ -20,14 +20,14 @@ export default defineConfig({
         orientation: 'any',
         icons: [
           {
-            src: 'favicon.svg',
+            src: 'logo.png',
             sizes: '192x192',
-            type: 'image/svg+xml'
+            type: 'image/png'
           },
           {
-            src: 'favicon.svg',
+            src: 'logo.png',
             sizes: '512x512',
-            type: 'image/svg+xml'
+            type: 'image/png'
           }
         ]
       },
